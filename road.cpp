391fd980abccac5e129d1b15e37a50379d9b3369
barakavic <barakavictor397@gmail.com>
2025-02-25 11:55:13 +0300

    The road file which holds the road properties such  as the color as well as the lane markings

--- road.cpp
+++ road.cpp
@@ -1,84 +1,41 @@
-#include <GL/glew.h>
-#include <GL/glut.h>
-#include <GL/glu.h>
-#include <iostream>
-#include <cmath>
+#include "road.h"
 
-//window dimensions
-const int WIDTH = 800, HEIGHT = 600;
+Road::Road(float width): roadWidth(width){}
 
-// Function to draw the straigh road
-void drawRoad(){
-    glColor3f(0.2f, 0.2f, 0.2f);//Dark gray road color
+void Road::draw(){
+    glColor3f(0.3f, 0.3f, 0.3f);// Dark gray road
     glBegin(GL_QUADS);
-        glVertex3f(-0.5f, -1.0f, 0.0f);
-        glVertex3f(0.5f, -1.0f, 0.0f);
-        glVertex3f(0.5f, 1.0f, 0.0f);
-        glVertex3f(-0.5f, 1.0f, 0.0f);
-    glEnd();
-
-}
-
-//Dashed lane markings
-
-void drawLaneMarkings(){
-    glColor3f(1.0f, 1.0f, 1.0f);//White dashed line color
-    glLineWidth(5.0f);
-
-    glEnable(GL_LINE_STIPPLE);
-    glLineStipple(4, 0xAAAA);
-
-    glBegin(GL_LINES);
-        glVertex3f(0.0f, -1.0f, 0.0f);
-        glVertex3f(0.0f, 1.0f, 0.0f);
+        glVertex2f(-roadWidth, -1.0f);
+        glVertex2f(roadWidth, -1.0f);
+        glVertex2f(roadWidth, 1.0f);
+        glVertex2f(-roadWidth, 1.0f);
 
     glEnd();
 
-    glDisable(GL_LINE_STIPPLE);
-}
+    glColor3f(1.0f, 1.0f, 1.0f);
+    float dashLegnth = 0.2f;
+    float gap = 0.2f;
+    float dashWidth = 0.02f;
 
-//Display Function
+    for (float y = -1.0f; y<1.0f; y+= dashLegnth + gap){
+        glBegin(GL_QUADS);
+            glVertex2f(-dashWidth, y);
+            glVertex2f(dashWidth, y);
+            glVertex2f(dashWidth, y + dashLegnth);
+            glVertex2f(-dashWidth, y + dashLegnth);
 
-void display(){
+        glEnd();
+            
+            
 
-    glClear(GL_COLOR_BUFFER_BIT);
 
-    glLoadIdentity();
-
-    drawRoad();
-    drawLaneMarkings();
-
-    glutSwapBuffers();
-
-}
-
-// OpenGl initialisation 
-void init(){
-
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);//Black Bg
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);// Set Coordinates
-
-}
-
-//Main Method
-
-int main(int argc, char** argv){
-
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(WIDTH, HEIGHT);
-    glutCreateWindow("Straight Road with Lane Markings");
-
-    glewInit();
-    init();
-
-    glutDisplayFunc(display);
-    glutMainLoop();
-
-    return 0;
-
+    }
 
 
 }
+
+
+
+
+
+
